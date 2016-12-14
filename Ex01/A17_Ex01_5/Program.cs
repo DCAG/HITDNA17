b83503f04c5426cd78a4c45841877f22ae3898dd
shlomit <shlomit__levy@Walla.co.il>
2016-12-14 19:38:53 +0200

    finish Ex01_1

--- Ex01/A17_Ex01_5/Program.cs
+++ Ex01/A17_Ex01_5/Program.cs
@@ -4,15 +4,15 @@ using System.Text;
 
 namespace A17_Ex01_5
 {
-    class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
             NumbersStatictics();
-            PrintProgramExitMessage();
+            printProgramExitMessage();
         }
 
-        private static void PrintProgramExitMessage()
+        private static void printProgramExitMessage()
         {
             Console.WriteLine("{0}Press any key to exit...", Environment.NewLine);
             Console.ReadLine();
@@ -20,28 +20,31 @@ namespace A17_Ex01_5
 
         private static void NumbersStatictics()
         {
-            int userInputNumber = ReadInputNumber();
-            GetBiggestDigit(userInputNumber);
-            GetSmallest(userInputNumber);
-            CountBiggerThanOncePlaceDigit();
+            string userInputNumber = readInputNumber();
+
+            Console.WriteLine("The biggest digit: {0}", getBiggestDigit(userInputNumber));
+            Console.WriteLine("The smallest digit: {0}", getSmallestDigit(userInputNumber));
+            Console.WriteLine("The count of digits bigger than the ones place digit: {0}", countBiggerThanOnesPlaceDigit(userInputNumber));
+            Console.WriteLine("The count of digits smaller than the ones place digit: {0}", countSmallerThanOnesPlaceDigit(userInputNumber));
         }
 
-        private static int ReadInputNumber()
+        private static string readInputNumber()
         {
             string number;
             bool tryAgain = true;
+
             do
             {
                 Console.WriteLine("Please enter a 7-digit (positive) number:");
                 number = Console.ReadLine();
-                int tempNumber; //discarded after use in int.TryParse()
+                int tempNumber; // discarded after use in int.TryParse()
                 if (number.Length != 7)
                 {
-                    Console.WriteLine("Invalid input! More than or less than 7 characters were entered.");
+                    Console.WriteLine("Invalid input! More than or less than 7 characters were entered.{0}", Environment.NewLine);
                 }
-                else if (!int.TryParse(number, out tempNumber))
+                else if (!int.TryParse(number, out tempNumber) || char.GetNumericValue(number[0]) == 0)
                 {
-                    Console.WriteLine("Invalid input! Invalid number was entered.");
+                    Console.WriteLine("Invalid input! Invalid number was entered.{0}", Environment.NewLine);
                 }
                 else
                 {
@@ -49,16 +52,74 @@ namespace A17_Ex01_5
                 }
             }
             while (tryAgain);
+
             return number;
         }
 
-        private static void PrintStatistics(string[] i_NumbersAsStrings, int[] i_NumbersAsIntegers, string[] i_NumbersAsBinary)
+        private static int getBiggestDigit(string i_Number)
         {
-            Console.WriteLine("{0}Statistics on given input:{0}", Environment.NewLine);
-            Console.WriteLine("Average amount of binary digits: {0:0.00}", AvgAmountOfDigits(i_NumbersAsBinary));
-            Console.WriteLine("Number of Sequences monotinically increasing: {0}", CountMonotonicSequences(i_NumbersAsStrings, SequenceBehavior.Increasing));
-            Console.WriteLine("Number of Sequences monotinically decreasing: {0}", CountMonotonicSequences(i_NumbersAsStrings, SequenceBehavior.Decreasing));
-            Console.WriteLine("Average value: {0:0.00}", CalculateAverage(i_NumbersAsIntegers));
+            int biggestDigit = (int)char.GetNumericValue(i_Number[0]);
+
+            for (int i = 1; i < i_Number.Length; i++)
+            {
+                int digitNumericValue = (int)char.GetNumericValue(i_Number[i]);
+
+                if (digitNumericValue > biggestDigit)
+                {
+                    biggestDigit = digitNumericValue;
+                }
+            }
+
+            return biggestDigit;
+        }
+
+        private static int getSmallestDigit(string i_Number)
+        {
+            int smallestDigit = (int)char.GetNumericValue(i_Number[0]);
+
+            for (int i = 1; i < i_Number.Length; i++)
+            {
+                int digitNumericValue = (int)char.GetNumericValue(i_Number[i]);
+
+                if (digitNumericValue < smallestDigit)
+                {
+                    smallestDigit = digitNumericValue;
+                }
+            }
+
+            return smallestDigit;
+        }
+
+        private static int countBiggerThanOnesPlaceDigit(string i_Number)
+        {
+            int onesPlaceDigit = (int)char.GetNumericValue(i_Number[i_Number.Length - 1]);
+            int counter = 0;
+
+            for (int i = 0; i < i_Number.Length - 1; i++)
+            {
+                if ((int)char.GetNumericValue(i_Number[i]) > onesPlaceDigit)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        private static int countSmallerThanOnesPlaceDigit(string i_Number)
+        {
+            int onesPlaceDigit = (int)char.GetNumericValue(i_Number[i_Number.Length - 1]);
+            int counter = 0;
+
+            for (int i = 0; i < i_Number.Length - 1; i++)
+            {
+                if ((int)char.GetNumericValue(i_Number[i]) < onesPlaceDigit)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
         }
     }
 }
