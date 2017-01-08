15a5a4002498a171dab003891dcc261b69b36bdd
Amir Granot <digiclick@hotmail.com>
2017-01-09 01:11:02 +0200

    Updated GameBoard class and Player class. The board is now called 'GridBoard' (feels more comfortable to use X and Y in parameters) and is now a array of Nullable<bool>. null is an empty square. true is white square. false is black square.

--- Ex02/Ex02_Othelo/GameBoard.cs
+++ Ex02/Ex02_Othelo/GameBoard.cs
@@ -1,40 +1,200 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Ex02_Othelo
 {
     class GameBoard
     {
-        char[,] m_Board;
-        public char[,] Board
+        private const bool v_White = true; //Black = false
+        private bool?[,] m_GridBoard;
+
+        public bool?[,] Board
         {
-            get { return m_Board; }
+            get
+            {
+                return m_GridBoard;
+            }
         }
 
-        public GameBoard(int size)
+        public GameBoard(int i_Size)
         {
-            m_Board = new char[size, size];
+            if ((2 < i_Size) && (i_Size % 2 == 0) && (i_Size == 6 || i_Size == 8))
+            {
+                m_GridBoard = new bool?[i_Size, i_Size];
+            }
+            else
+            {
+                throw new Exception("Illegal GameBoard size");
+            }
         }
 
-        private string[] GetAvailableMoves(char i_char)
+        private List<string> getAvailableMoves(bool i_WhitePlayer)
         {
-            
+            /* inspired by:
+             * https://user.xmission.com/~sgigo/elec/ocreversi/legalmoves.html
+             */
+            List<string> availableMoves = new List<string>();
+            for (int i = 0; i < m_GridBoard.GetLength(0); i++)
+            {
+                for (int j = 0; i < m_GridBoard.GetLength(1); j++)
+                {
+                    if(isLegalMove(i, j, i_WhitePlayer))
+                    {
+                        availableMoves.Add(ParseCoordinatesToStr(i,j));
+                    }
+                }
+            }
+            return availableMoves;
         }
 
-        internal bool HasMoves(Player i_Player)
+        private string ParseCoordinatesToStr(int i_Row, int i_Column)
         {
-            return GetAvailableMoves(i_Player.Symbol).Length > 0;
+            return string.Format("{1}{0}", i_Row, (char)('A' + i_Column));
         }
 
-        internal void SetInitialBoard()
+        private bool tryParseStrToCoordinates(string i_str, out int o_Row, out int o_Column)
         {
-            throw new NotImplementedException();
+            Regex pattern = new Regex(@"^(?<column>[A-Z]{1})(?<row>\d{1})$");
+            MatchCollection matches = pattern.Matches(i_str);
+            bool success = false;
+
+            o_Row = -1;
+            o_Column = -1;
+
+            foreach (Match match in matches)
+            {
+                if (match.Success)
+                {
+                    o_Row = int.Parse(match.Groups["row"].Value);
+                    o_Column = char.Parse(match.Groups["column"].Value) - 'A';
+                    success = true;
+                }
+            }
+            /*
+            o_Column = i_str[0] - 'A';
+            o_Row = i_str[1] - '0';
+            */
+            return success;
         }
 
-        internal bool UpdateBoard(string move)
+        private bool isLegalMove(int i_X, int i_Y, bool i_WhitePlayer)
         {
-            throw new NotImplementedException();
+            const int k_Up = -1, k_Left = -1, k_Down = 1, k_Right = 1, k_Stay = 0;
+
+            return TestMoveDirection(i_X, i_Y, k_Up,   k_Left,  i_WhitePlayer) || //North-West 
+                   TestMoveDirection(i_X, i_Y, k_Stay, k_Left,  i_WhitePlayer) || //North
+                   TestMoveDirection(i_X, i_Y, k_Down, k_Left,  i_WhitePlayer) || //North-East 
+                   TestMoveDirection(i_X, i_Y, k_Down, k_Stay,  i_WhitePlayer) || //East 
+                   TestMoveDirection(i_X, i_Y, k_Down, k_Right, i_WhitePlayer) || //South-East
+                   TestMoveDirection(i_X, i_Y, k_Stay, k_Right, i_WhitePlayer) || //South
+                   TestMoveDirection(i_X, i_Y, k_Up,   k_Right, i_WhitePlayer) || //South-West
+                   TestMoveDirection(i_X, i_Y, k_Up,   k_Stay,  i_WhitePlayer);   //West
+        }
+
+        private bool TestMoveDirection(int i_X, int i_Y, int i_DeltaX, int i_DeltaY, bool i_WhitePlayer)
+        {
+            bool validDirection = true;
+            int numFlipped = 0;
+            do
+            {
+                i_X += i_DeltaX;
+                i_Y += i_DeltaY;
+
+                if (isOutOfBounds(i_X, i_Y) || isEmptySquare(i_X, i_Y))
+                {
+                    validDirection = false;
+                    break;
+                }
+                else if(m_GridBoard[i_X, i_Y] == !i_WhitePlayer)
+                {
+                    numFlipped++;
+                }
+            }
+            while (m_GridBoard[i_X, i_Y] != i_WhitePlayer);
+
+            validDirection = validDirection && numFlipped > 0;
+
+            return validDirection;
+        }
+
+        private bool isEmptySquare(int i_X,int i_Y)
+        {
+            return m_GridBoard[i_X, i_Y] == null;
+        }
+
+        private bool isOutOfBounds(int i_X,int i_Y)
+        {
+            return i_X < 0 || m_GridBoard.GetLength(0) < i_X ||
+                   i_Y < 0 || m_GridBoard.GetLength(1) < i_Y;
+        }
+
+        public bool HasMoves(Player i_Player)
+        {
+            return getAvailableMoves(i_Player.WhiteDisc).Count > 0;
+        }
+
+        public void SetInitialBoard()
+        {
+            for (int i = 0; i < m_GridBoard.GetLength(0); i++)
+            {
+                for (int j = 0; i < m_GridBoard.GetLength(1); j++)
+                {
+                    m_GridBoard[i, j] = null;
+                }
+            }
+
+            int boardCenterPosition = m_GridBoard.GetLength(0) / 2;
+
+            m_GridBoard[boardCenterPosition - 1, boardCenterPosition - 1] =  v_White; //up,left
+            m_GridBoard[boardCenterPosition - 1, boardCenterPosition    ] = !v_White; //left
+            m_GridBoard[boardCenterPosition    , boardCenterPosition    ] =  v_White; //base square
+            m_GridBoard[boardCenterPosition    , boardCenterPosition - 1] = !v_White; //up
+        }
+
+        public bool UpdateBoard(string i_Square, bool i_WhitePlayer)
+        {
+            int row, column;
+            return tryParseStrToCoordinates(i_Square, out row, out column) && UpdateBoard(row, column, i_WhitePlayer);
+        }
+
+        public bool UpdateBoard(int i_X, int i_Y, bool i_WhitePlayer)
+        {
+            const int k_Up = -1, k_Left = -1, k_Down = 1, k_Right = 1, k_Stay = 0;
+
+            return tryFlipInDirection(i_X, i_Y, k_Up,   k_Left,  i_WhitePlayer) || //North-West 
+                   tryFlipInDirection(i_X, i_Y, k_Stay, k_Left,  i_WhitePlayer) || //North
+                   tryFlipInDirection(i_X, i_Y, k_Down, k_Left,  i_WhitePlayer) || //North-East 
+                   tryFlipInDirection(i_X, i_Y, k_Down, k_Stay,  i_WhitePlayer) || //East 
+                   tryFlipInDirection(i_X, i_Y, k_Down, k_Right, i_WhitePlayer) || //South-East
+                   tryFlipInDirection(i_X, i_Y, k_Stay, k_Right, i_WhitePlayer) || //South
+                   tryFlipInDirection(i_X, i_Y, k_Up,   k_Right, i_WhitePlayer) || //South-West
+                   tryFlipInDirection(i_X, i_Y, k_Up,   k_Stay,  i_WhitePlayer);   //West
+        }
+
+        private bool tryFlipInDirection(int i_X, int i_Y, int i_DeltaX, int i_DeltaY, bool i_WhitePlayer)
+        {
+            bool success = false;
+            if (TestMoveDirection(i_X, i_Y, i_DeltaX, i_DeltaY, i_WhitePlayer))
+            {
+                flipInDirection(i_X, i_Y, i_DeltaX, i_DeltaY, i_WhitePlayer); //West
+                success = true;
+            }
+
+            return success;
+        }
+
+        private void flipInDirection(int i_X, int i_Y, int i_DeltaX, int i_DeltaY, bool i_WhitePlayer)
+        {
+            i_X += i_DeltaX;
+            i_Y += i_DeltaY;
+            while (m_GridBoard[i_X, i_Y] != i_WhitePlayer)
+            {
+                m_GridBoard[i_X, i_Y] = i_WhitePlayer; //flip
+                i_X += i_DeltaX;
+                i_Y += i_DeltaY;
+            }
         }
     }
 }
--- Ex02/Ex02_Othelo/Player.cs
+++ Ex02/Ex02_Othelo/Player.cs
@@ -6,12 +6,39 @@ namespace Ex02_Othelo
 {
     class Player
     {
-        internal readonly bool IsComputer;
-        internal readonly char Symbol;
-        private string m_Name;
-        public string Name {
-            get { return m_Name; }
-            set { m_Name = value; }
+        private readonly bool r_IsComputer;
+        private readonly bool m_WhiteDisc;
+        private readonly string m_Name;
+
+        public bool IsComputer
+        {
+            get
+            {
+                return r_IsComputer;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return m_Name;
+            }
+        }
+
+        public bool WhiteDisc
+        {
+            get
+            {
+                return m_WhiteDisc;
+            }
+        }
+
+        public Player(string i_Name, bool i_White, bool i_IsComputer)
+        {
+            m_Name = i_Name;
+            m_WhiteDisc = i_White;
+            r_IsComputer = i_IsComputer;
         }
 
         internal string GetRandomMove()
