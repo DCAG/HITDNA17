3e7d1e7667efe98af95c355a81273a46c5ba3b96
Amir Granot <digiclick@hotmail.com>
2017-01-27 20:46:20 +0200

    a start of Ex04

---
+++ Ex04/Ex04.Menus.Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex04.Menus.Test
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Interfaces.MainMenu interfacesMenu = new Interfaces.MainMenu();
+            Delegates.MainMenu delegatesMenu = new Delegates.MainMenu();
+        }
+    }
+}
